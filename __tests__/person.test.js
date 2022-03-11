d81a46e0d466fa9de51301970f1221dfed4aa966
Marcus <marcusferreira23@gmail.com>
2022-03-11 10:03:23 -0800

    refactor first Person test with correct information for testing.

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -2,10 +2,10 @@ import Person from './../src/js/person.js'
 
 describe('Person', () => {
 
-  test('should correctly create a Person object with age relative to Earth', () => {
-    const person = new Person();
-    person.age();
-    expect(person.age).toEqual(20);
+  test('should correctly create a Person object with name and age relative to Earth', () => {
+    const person = new Person("Buzz", 20);
+    expect(person.name)
+    expect(person.earthAge).toEqual(20);
   });
 
 });
